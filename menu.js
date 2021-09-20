d521d618920e523b6d9e43569bc8828a600d9a31
Hanna Masanjika <hanna.masanjika@gmail.com>
2021-09-20 10:35:55 +0200

    Added menu

---
+++ menu.js
@@ -0,0 +1,82 @@
+const MENU = [
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    },
+    {
+        type = 'Pizza',
+        name = 'Marinara',
+        categories = ['Vegetarian', 'Vegan'],
+        ingredients = ['tomato', 'garlic', 'oregano'],
+        price = 6,
+        image = "https://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/02/m5.jpg"
+    },
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    },
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    },
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    },
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    },
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    },
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    },
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    },
+    {
+        type = 'Pizza',
+        name = 'Margherita',
+        categories = ['Vegetarian'],
+        ingredients = ['mozzarella', 'tomato', 'basil'],
+        price = 8,
+        image = "https://assets.afcdn.com/recipe/20200206/107152_origin.png"
+    }
+]
